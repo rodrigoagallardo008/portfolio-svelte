7c09f388b2c371eff1bf3f750ba77c6751e13ab8
Rodrigo Gallardo <rodrigo.gallardo008@gmail.com>
2026-05-06 17:49:27 -0400

    Update svelte.config.js

--- .github/workflows/svelte.config.js
+++ .github/workflows/svelte.config.js
@@ -3,11 +3,17 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-		},
-	},
+			base: process.env.BASE_PATH || '',
+		}
+	}
 };
 
 export default config;
